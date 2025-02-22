7e562d08516f2d1eabf8d65f19a75623babc289d
nico9696 <nicholasbenedictdysy@gmail.com>
2025-02-22 23:25:32 +0800

    added urls for both ingredients in the app

--- ledger/urls.py
+++ ledger/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("recipes/list", views.show_recipes_list, name="recipe_list"),
-    # path("recipe/1", views.show_ingredients_1, name="show_ingredients_1"),
-    # path("recipe/2", views.show_ingredients_2, name="show_ingredients_2"),
+    path("recipe/1", views.show_ingredients_1, name="show_ingredients_1"),
+    path("recipe/2", views.show_ingredients_2, name="show_ingredients_2"),
 ]
