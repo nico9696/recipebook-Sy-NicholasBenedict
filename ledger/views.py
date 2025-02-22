860c46df7b7273c3947a0bc6575266b8152b6e76
nico9696 <nicholasbenedictdysy@gmail.com>
2025-02-22 23:21:49 +0800

    made function to show ingredients of recipe 1

--- ledger/views.py
+++ ledger/views.py
@@ -13,3 +13,29 @@ def show_recipes_list(request):
     ]
     
     return render(request, "ledger/recipes_list.html", {"recipes": recipes})
+
+def show_ingredients_1(request):
+    ingredients = [
+        {
+            "name": "tomato",
+            "quantity": "3pcs"
+        },
+        {
+            "name": "onion",
+            "quantity": "1pc"
+        },
+        {
+            "name": "pork",
+            "quantity": "1kg"
+        },
+        {
+            "name": "water",
+            "quantity": "1L"
+        },
+        {
+            "name": "sinigang mix",
+            "quantity": "1 packet"
+        }
+    ]
+
+    return render(request, "ledger/ingredients.html", {"ingredients": ingredients})
