71b2d2082159a1cabb80e2b3bdb07591eaf1eabe
nico9696 <nicholasbenedictdysy@gmail.com>
2025-03-01 21:24:43 +0800

    Added RecipeIngredient and Recipe models

--- ledger/models.py
+++ ledger/models.py
@@ -6,3 +6,14 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return str(self.id) 
+    
+class Recipe(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return str(self.id) 
+
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=64)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="recipe")
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="ingredient") 
