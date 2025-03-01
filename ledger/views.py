9cfd0f4c1dc4f8ba27bcbba4ebc1b1c48ed076a0
nico9696 <nicholasbenedictdysy@gmail.com>
2025-03-01 21:45:55 +0800

    modified ingredients view to now use Ingredient model:

--- ledger/views.py
+++ ledger/views.py
@@ -3,3 +3,6 @@ from .models import Ingredient, Recipe, RecipeIngredient
 
 def show_recipes_list(request):
     return render(request, "ledger/recipes_list.html", {"recipes": Recipe.objects.all()})
+
+def show_ingredients(request, num):
+    return render(request, "ledger/ingredients.html", {"ingredients": RecipeIngredient.objects.filter(recipe=num)})
