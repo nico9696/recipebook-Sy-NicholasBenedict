10cb7505890e25f81485f72b15a15a7f90a7bab3
nico9696 <nicholasbenedictdysy@gmail.com>
2025-03-01 21:56:12 +0800

    created and customized admin panel for Ingredient

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from .models import Ingredient, Recipe, RecipeIngredient
+from .models import Recipe, Ingredient, RecipeIngredient
 
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
     search_fields = ('name', )
-    list_display = ('id', 'name')  # Shows readable recipe names
+    list_display = ('id', 'name')  
+
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+    search_fields = ('name', )
+    list_display = ('id', 'name')  
 
 admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
