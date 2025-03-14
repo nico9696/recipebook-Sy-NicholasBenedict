422bf58cec730f1a9c9e678c9938e581114a6143
nico9696 <nicholasbenedictdysy@gmail.com>
2025-03-15 00:47:47 +0800

    added log-in url in settings

--- ledger/views.py
+++ ledger/views.py
@@ -5,12 +5,20 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 
+@login_required
 def show_recipes_list(request):
-    if not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse("login"))
     return render(request, "ledger/recipes_list.html", {"recipes": Recipe.objects.all()})
 
+@login_required
 def show_ingredients(request, num):
-    if not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse("login"))
     return render(request, "ledger/ingredients.html", {"ingredients": RecipeIngredient.objects.filter(recipe=num)})
+	
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("recipe_list"))
+    return render(request, "ledger/login.html")
