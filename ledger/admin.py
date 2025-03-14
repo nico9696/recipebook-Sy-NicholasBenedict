945c6033d3ced3df056a891949230cc3314ef3d5
nico9696 <nicholasbenedictdysy@gmail.com>
2025-03-14 17:52:58 +0800

    fixed some issues in admin related to profile and user

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Recipe, Ingredient, RecipeIngredient, Profile
 from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 # used Chatgpt for syntax of this particular class
 class RecipeIngredientInline(admin.TabularInline):  
@@ -28,9 +29,12 @@ class ProfileInline(admin.StackedInline):
     model = Profile
     can_delete = False
 
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
     inlines = [ProfileInline]
 
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
