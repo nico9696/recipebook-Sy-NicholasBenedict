9a02030c62b55d95b81a69f9fbc3e09a251a31b7
nico9696 <nicholasbenedictdysy@gmail.com>
2025-03-14 23:42:36 +0800

    added login and logout urls

--- ledger/views.py
+++ ledger/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
 from .models import Ingredient, Recipe, RecipeIngredient
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
 
-@login_required
 def show_recipes_list(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
     return render(request, "ledger/recipes_list.html", {"recipes": Recipe.objects.all()})
 
 def show_ingredients(request, num):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
     return render(request, "ledger/ingredients.html", {"ingredients": RecipeIngredient.objects.filter(recipe=num)})
