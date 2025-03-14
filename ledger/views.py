b72649167c3b0a0e0c54258d4b6fbd9dc0da511b
nico9696 <nicholasbenedictdysy@gmail.com>
2025-03-14 18:08:03 +0800

    added functionality to display author in recipe list

--- ledger/views.py
+++ ledger/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Ingredient, Recipe, RecipeIngredient
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def show_recipes_list(request):
     return render(request, "ledger/recipes_list.html", {"recipes": Recipe.objects.all()})
 
