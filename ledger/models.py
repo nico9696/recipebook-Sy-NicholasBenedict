6d5a0b3253b0a48a5d5b98a15dde48bdab804172
nico9696 <nicholasbenedictdysy@gmail.com>
2025-03-14 17:18:12 +0800

    created profile model

--- ledger/models.py
+++ ledger/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=64)
@@ -16,3 +17,9 @@ class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=64)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="recipe")
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="ingredient") 
+
+# Create a Profile model by extending the default User model
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	name = models.CharField(max_length=50)
+	bio = models.CharField(max_length=255)
