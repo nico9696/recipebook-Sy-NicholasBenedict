53d0fe8fab9ba8d8ef9dba6ead76a47f514ff922
nico9696 <nicholasbenedictdysy@gmail.com>
2025-04-07 17:31:28 +0800

    Protected SECRET_KEY by using a .env file

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qp0h64=*c8mw6r34udeqw88gc1y6&@f-#68hz=ek*soun--my*'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
